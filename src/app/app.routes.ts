fe5a859eb411c8b3de8b0ebc65d4aec57f578d37
Chispacion <sebastiangama111@gmail.com>
2026-04-28 00:09:44 -0500

    fix errores de build

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { ListaContactosComponent } from './components/lista-contactos/lista-contactos';
+import { RenderMode } from '@angular/ssr';
 import { FormularioContactoComponent } from './components/formulario-contacto/formulario-contacto';
 
 export const routes: Routes = [
   { path: '', component: ListaContactosComponent },
   { path: 'nuevo', component: FormularioContactoComponent },
-  { path: 'editar/:id', component: FormularioContactoComponent },
+  { 
+    path: 'editar/:id', 
+    component: FormularioContactoComponent,
+    data: { renderMode: RenderMode.Client }
+  },
 ];
